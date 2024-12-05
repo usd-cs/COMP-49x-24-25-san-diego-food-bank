fe2e92817488e0d6efbd58a00e85a11815b044bf
Brendan Bost <bbost@sandiego.edu>
2024-12-05 15:19:20 -0800

    Began adding basic test implementation for the login view and navigation bar

--- sd_food_bank_ai_bot/hello_world/tests.py
+++ sd_food_bank_ai_bot/hello_world/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+    def setup(self):
+        """
+        Set up a test client and test user
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(username='user1', password='pass123')
+
+    def test_login_view_get(self):
+        """
+        Test GET request for the login view
+        """
