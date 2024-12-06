b8893bc8521ae368422c995e808f256247a602e8
Brendan Bost <bbost@sandiego.edu>
2024-12-06 10:11:29 -0800

    Created login template file, started unit tests for the login view, and added the login view.

--- sd_food_bank_ai_bot/admin_panel/views.py
+++ sd_food_bank_ai_bot/admin_panel/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def login_view(request):
@@ -9,7 +10,15 @@ def login_view(request):
     This view will display the login form for users to input their username and password. It will
     validate the user's credentials and log them in, then redirect them to the home page.
     """
-    return render(request, 'admin_panel/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect() # No Home page yet, need to add redirect later
+        else: 
+            form = AuthenticationForm()
+        return render(request, 'admin_panel/login.html')
 
 def faq_page_view(request):
     return render(request, 'faq_page.html')
