9947fc10452147dffbbfe4dd27f77a7b8c3ddd3f
Evan Scott <evan.scott@dc.rr.com>
2024-12-09 13:43:21 -0800

    Implemented django models: FAQ, Admin, Tag. Added local connection to postgresql db for simulated functionality of admin panel

--- sd_food_bank_ai_bot/admin_panel/models.py
+++ sd_food_bank_ai_bot/admin_panel/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Admin(AbstractUser):
+    """Table for storing information on admins"""
+    
+class Tag(models.Model):
+    """Table for storing tags to assign to FAQs"""
+    name = models.TextField()
 
 class FAQ(models.Model):
+    """Table for storing FAQs"""
     question = models.TextField()
     answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True) 
+    tags = models.ManyToManyField(Tag)
     updated_at = models.DateTimeField(auto_now=True) # Automatically update when saving
