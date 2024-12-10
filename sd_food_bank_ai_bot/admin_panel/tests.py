31eb056324e7a36bc3b2fa4630ccc9d53bfb2531
kierankenn <kierankennedy@sandiego.edu>
2024-12-09 16:19:15 -0800

    Added tests for FAQ page template and loading FAQs

--- sd_food_bank_ai_bot/admin_panel/tests.py
+++ sd_food_bank_ai_bot/admin_panel/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User 
 from django.urls import reverse
-from admin_panel.models import Admin
+from admin_panel.models import Admin, FAQ
 
 
 class LoginViewsTestCase(TestCase):
@@ -25,4 +25,30 @@ class LoginViewsTestCase(TestCase):
         """Test the POST request with invalid login credentials"""
         response = self.client.post(reverse('login'), {'username': 'user3', 'password': 'pass321'})
         self.assertTemplateUsed(response, 'login.html')
+
+
+class FAQPageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.faq_1 = FAQ.objects.create(question="When does the food bank open?", answer="The food bank is open Monday-Friday from ():00 AM to 5:00 PM")
+        self.faq_2 = FAQ.objects.create(question="How can I have access to the food bank client choice center?", answer="To have access to the client choice center, you must have scheduled an appointment")
+    
+    def test_get_request(self):
+        """Test the GET request for FAQ view"""
+        response = self.client.get(reverse('faq_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faq_page.html')
+    
+    def test_items_displayed(self):
+        response = self.client.get(reverse('faq_page'))
+
+        # Check first FAQ appears
+        self.assertContains(response, "When does the food bank open?")
+        self.assertContains(response, "The food bank is open Monday-Friday from ():00 AM to 5:00 PM")
+
+        # Check second FAQ appears
+        self.assertContains(response, "How can I have access to the food bank client choice center?")
+        self.assertContains(response, "To have access to the client choice center, you must have scheduled an appointment")
+
+    
         
