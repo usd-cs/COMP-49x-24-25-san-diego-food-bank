d82a623c73580324dd126ab43016098b12ec08f3
kierankenn <kierankennedy@sandiego.edu>
2025-03-05 18:50:48 -0800

    Reverted changes; Only searching for phone number in post request

--- sd_food_bank_ai_bot/admin_panel/tests/phone_service_schedule_tests.py
+++ sd_food_bank_ai_bot/admin_panel/tests/phone_service_schedule_tests.py
@@ -1,71 +1,35 @@
-from django.test import TestCase, Client, RequestFactory
+from django.test import TestCase, RequestFactory
 from admin_panel.views.phone_service_schedule import *
-from unittest.mock import patch, MagicMock
-from django.http import HttpResponse
 
 
 class RequestPhoneNumberTests(TestCase):
     def setUp(self):
-        """Setup a request facotry for use during tests"""
+        """Setup a request factory for use during tests"""
         self.factory = RequestFactory()
     
-    def test_get_phone_number(self):
-        """Test the first prompt for getting a users phone number"""
-        self.client = Client()
-        response = self.client.get("/get_phone_number")
-        content = response.content.decode('utf-8')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Would you like to use the number you're calling from, or provide a different one?", content)
-
-    @patch("admin_panel.views.phone_service_schedule.OpenAI")
-    @patch("admin_panel.views.phone_service_schedule.get_current_phone_number")
-    @patch("admin_panel.views.phone_service_schedule.get_other_phone_number")
-    def test_get_user_phone_preference_current(self, mock_get_other_phone_number, mock_get_current_phone_number, mock_openai):
-        """Test when the user responds with wanting to use their current phone number."""
-        mock_client = MagicMock()
-        mock_openai.return_value = mock_client
-        mock_client.chat.completions.create.return_value = MagicMock(
-            choices=[MagicMock(message=MagicMock(content="CURRENT"))]
-        )
-
-        mock_get_other_phone_number.return_value("+16293641295")
-        mock_get_current_phone_number.return_value("+29999999999")
-        request = self.factory.post("/get_user_phone_preference/") # used only as parameter
-
-        ret_value = get_user_phone_preference(request, "tmp")
-
-        mock_get_other_phone_number.assert_not_called
-        mock_get_current_phone_number.assert_called()
-        self.assertEqual(ret_value, "+29999999999")
-
-    @patch("admin_panel.views.phone_service_schedule.OpenAI")
-    @patch("admin_panel.views.phone_service_schedule.get_current_phone_number")
-    @patch("admin_panel.views.phone_service_schedule.get_other_phone_number")
-    def test_get_user_phone_preference_other(self, mock_get_other_phone_number, mock_get_current_phone_number, mock_openai):
-        """Test when the user responds with wanting to use a different phone number."""
-        mock_client = MagicMock()
-        mock_openai.return_value = mock_client
-        mock_client.chat.completions.create.return_value = MagicMock(
-            choices=[MagicMock(message=MagicMock(content="OTHER"))]
-        )
-
-        mock_get_other_phone_number.return_value("+16293641295")
-        mock_get_current_phone_number.return_value("+29999999999")
-        request = self.factory.post("/get_user_phone_preference/") # used only as parameter
-
-        ret_value = get_user_phone_preference(request, "tmp")
-
-        mock_get_other_phone_number.assert_called
-        mock_get_current_phone_number.assert_not_called()
-        self.assertEqual(ret_value, "+16293641295")
-    
-    """
-    def test_get_current_phone_number(self):
+    def test_get_phone_number_US_valid(self):
+        """Test getting the phone number the user is calling from in the US when it is valid"""
         phone_number = "+16192222222"
         
-        request = self.factory.post("/get_current_phone_number/", {"From": f"{phone_number}"})
-        response = get_current_phone_number(request)
+        request = self.factory.post("/answer/", {"From": f"{phone_number}"})
+        response = get_phone_number(request)
         
-        self.assertIn(phone_number, response.content.decode())
-    """
+        self.assertEqual(phone_number, response)
+
+    def test_get_phone_number_MEX_valid(self):
+        """Test getting the phone number the user is calling from in Mexico when it is valid"""
+        phone_number = "+521212341234"
+
+        request = self.factory.post("/answer/", {"From": f"{phone_number}"})
+        response = get_phone_number(request)
+        
+        self.assertEqual(phone_number, response)
+
+    def test_get_phone_number_invalid(self):
+        """Test getting the phone number the user is calling from when it is invalid"""
+        phone_number = "UNKNOWN"
+        
+        request = self.factory.post("/answer/", {"From": f"{phone_number}"})
+        response = get_phone_number(request)
+
+        self.assertEqual(None, response)
--- sd_food_bank_ai_bot/admin_panel/views/phone_service_schedule.py
+++ sd_food_bank_ai_bot/admin_panel/views/phone_service_schedule.py
@@ -1,47 +1,22 @@
 from twilio.twiml.voice_response import VoiceResponse, Gather, Say
 from .phone_service_faq import get_response_sentiment
+from django.http import HttpResponse
+from openai import OpenAI
 import re
+
 
 TIMEOUT_LENGTH = 2 # The length of time the bot waits for a response
 
 def get_phone_number(request):
     """
-    Prompts the user by asking if the current phone number should be used or not.
-    """
-    caller_response = VoiceResponse()
-
-    gather = Gather(input="speech", timeout=TIMEOUT_LENGTH, action="/get_user_phone_preference/")
-    gather.say("Would you like to use the number you're calling from, or provide a different one?")
-    caller_response.append(gather)
-
-    return HttpResponse(str(caller_response), content_type='text/xml')
-
-def get_user_phone_preference(request, sentence):
-    """
-    Gets whether the user wants to use the current number or a different number.
-    """
-    # Query GPT for intent
-    client = OpenAI()
-    system_prompt = "Based on the following message, respond whether the user is asking to use their CURRENT phone number or OTHER. Respond only with CURRENT or OTHER."
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": sentence}
-        ]
-    )   
-    response_pred = completion.choices[0].message.content
-
-def get_current_phone_number(request):
-    """
-    Gets the phone number that the user is calling from.
+    Gets the user phone number from the post header
     """
     caller_number = request.POST.get('From', '')
 
     #regex check
-    expression = "^\+[0-9]{11}$" # include + for country code
+    expression = "^\+[1-9]\d{1,14}$" # E.164 compliant phone numbers
     valid = re.match(expression, caller_number)
-
+    
     if valid:
-        return expression
+        return caller_number
     return None
