f94aa73ae89a41ee8b33e8c561119b7ad0f56888
kierankenn <kierankennedy@sandiego.edu>
2025-03-27 00:12:05 -0700

    Changed file names

--- sd_food_bank_ai_bot/admin_panel/tests/__init__.py
+++ sd_food_bank_ai_bot/admin_panel/tests/__init__.py
@@ -1,4 +1,4 @@
 from .admin_panel_faq_tests import *
 from .phone_service_faq_tests import *
 from .phone_service_schedule_tests import *
-from .phone_service_cancel_reschedule_tests import *
+from .phone_service_cancel_tests import *
